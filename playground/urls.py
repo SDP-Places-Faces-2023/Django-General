da84eaacb3d5a8e0986b3ef5d1157d19f1be40e9
HuseynJabbarov <huseyn.cabbarov434@gmail.com>
2023-02-12 12:29:36 +0400

    django-fastapi connection, fastapi image classification models connected

--- playground/urls.py
+++ playground/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.say_hello),
-    path('fff/', views.get_data_from_fastapi)
+    path('fff/', views.get_data_from_fastapi),
+    path('objectdetection/', views.get_object_detection, name='get_object_detection_results'),
+    path('objectclassification/', views.get_object_classification, name='get_object_detection_asdfasdf')
+
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import requests
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -21,3 +22,31 @@ async def get_data_from_fastapi(request):
         return JsonResponse(data)
     else:
         return JsonResponse({'error': 'Failed to retrieve data from FastAPI'}, status=500)
+
+
+@csrf_exempt
+def get_object_detection(request):
+    if request.method == 'POST':
+        url = 'http://127.0.0.1:8000/objectdetection/'
+        files = {'file': request.FILES['file'].read()}
+        response = requests.post(url, files=files)
+        if response.status_code == 200:
+            data = response.json()
+            return JsonResponse(data)
+        else:
+            return JsonResponse({'error': 'Failed to retrieve data from FastAPI'}, status=500)
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+@csrf_exempt
+def get_object_classification(request):
+    if request.method == 'POST':
+        url = 'http://127.0.0.1:8000/predict/image'
+        files = {'file': request.FILES['file'].read()}
+        response = requests.post(url, files=files)
+        if response.status_code == 200:
+            data = response.json()
+            return JsonResponse(data)
+        else:
+            return JsonResponse({'error': 'Failed to retrieve data from FastAPI'}, status=500)
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
+
