556c4a26b7e6be96efed79a159a663ac5ac7de65
huseyn <huseyn.cabbarov434@gmail.com>
2023-02-22 13:53:44 +0400

    Face recognition model connection established

--- playground/views.py
+++ playground/views.py
@@ -7,7 +7,6 @@ import requests
 import time
 
 
-
 async def get_data(request):
     data = get_data_from_fastapi(request)
     return render(request, 'template.html', {'data': data})
@@ -50,20 +49,25 @@ def get_object_classification(request):
             return JsonResponse({'error': 'Failed to retrieve data from FastAPI'}, status=500)
     return JsonResponse({'error': 'Invalid request method'}, status=400)
 
+
 @csrf_exempt
 def frame_post(request):
     if request.method == 'POST':
         url = 'http://127.0.0.1:8000/detect_faces/'
+        url_face = 'http://127.0.0.1:8000/recognize_faces/'
         files = {'file': request.FILES['file'].read()}
         response = requests.post(url, files=files)
-        #Get face coordinates, crop the face, send face to recognition API, return Employee ID
+        # Get face coordinates, crop the face, send face to recognition API, return Employee ID
 
         if response.status_code == 200:
             data = response.json()
             print(data)
+            if data != "No Face":
+                rec_response = requests.post(url_face, files=files)
+                face_data = rec_response.json()
+                print(data, face_data)
+                return JsonResponse(face_data, safe=False)
             return JsonResponse(data, safe=False)
         else:
             return JsonResponse({'error': 'Failed to retrieve data from FastAPI'}, status=500)
     return JsonResponse({'error': 'Invalid request method'}, status=400)
-
-
